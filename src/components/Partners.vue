d13db08a18bc0a692b02cf9def227d3e4cdad263
Ankur Nigam <ankur.nigam198@gmail.com>
2019-08-05 20:34:11 +0530

    images transparent

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="partners">
     <div class="page">
-      <h1 class="mainheading">partners</h1>
+      <h1 class="mainheading">our proud partners...</h1>
       <!-- <div>
         <img
           v-for="(brand, index) in internal.images"
@@ -13,6 +13,7 @@
       <vueper-slides
         autoplay
         :speed="1000"
+        :pauseOnHover="false"
         :slide-ratio="1 / 4"
         :visible-slides="4"
         :arrows="false"
@@ -55,8 +56,10 @@ export default {
 .vueperslide {
   background-repeat: no-repeat;
   background-position: center;
-  margin: 20px;
-  border-radius: 500px;
+  background-size: contain;
+  // margin: 20px;
+  z-index: -1;
+  // border-radius: 500px;
   // height: 500px;
 
   &__content-wrapper {
