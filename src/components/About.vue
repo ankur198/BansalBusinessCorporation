3437274f1529acdce6aa632ebae68c89154caeae
Ankur Nigam <ankur.nigam198@gmail.com>
2019-08-16 06:09:59 +0530

    odometer added

--- src/components/About.vue
+++ src/components/About.vue
@@ -22,17 +22,17 @@
         <div class="right">
           <div class="icon">
             <i class="material-icons">assignment_late</i>
-            <p>1000+</p>
+            <IOdometer class="iOdometer" :value="num1" />
             <h4>Partners</h4>
           </div>
           <div class="icon">
             <i class="material-icons">check_circle_outline</i>
-            <p>700+</p>
+            <IOdometer class="iOdometer" :value="num2" />
             <h4>Products</h4>
           </div>
           <div class="icon">
             <i class="material-icons">dashboard</i>
-            <p>20+</p>
+            <IOdometer class="iOdometer" :value="num3" />
             <h4>Years of trust</h4>
           </div>
         </div>
@@ -42,11 +42,49 @@
 </template>
 
 <script>
+import IOdometer from "vue-odometer";
+import "odometer/themes/odometer-theme-default.css";
+
 export default {
-  name: "about"
+  name: "about",
+  components: { IOdometer },
+  data: function() {
+    return {
+      num1: 0,
+      num2: 0,
+      num3: 0
+    };
+  },
+  methods: {
+    checkHeight: function(event) {
+      const itemHeight = document
+        .querySelector("#aboutPage")
+        .getBoundingClientRect();
+
+      if (itemHeight.top === 0) {
+        this.num1 = 1000;
+        this.num2 = 700;
+        this.num3 = 20;
+      } else {
+        this.num1 = 0;
+        this.num2 = 0;
+        this.num3 = 0;
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.checkHeight);
+  }
 };
 </script>
 
+<style lang="scss">
+.odometer-inside {
+  &::after {
+    content: "+";
+  }
+}
+</style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .page {
@@ -77,12 +115,14 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+    font-size: 1.5em;
+    font-weight: 800;
   }
   h4 {
     margin: 0;
   }
   i {
-    font-size: 3.5em;
+    font-size: 2.5em;
   }
 }
 .line {
